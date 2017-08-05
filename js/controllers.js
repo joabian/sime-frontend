ea5f80759fda2b4fde8c0f3bbb1b447b063936cd
kalinrex <biper_1_2@hotmail.com>
2017-08-04 18:06:52 -0600

    actualizacion

--- js/controllers.js
+++ js/controllers.js
@@ -63,10 +63,6 @@ function addArea($scope, $http)
         $log.error('Oops! Something went wrong while fetching the data.')
     });
 
-  
-
-
-
     $scope.saveArea = function () {
 
         var today = new Date();
@@ -101,13 +97,22 @@ function addArea($scope, $http)
 }
 
 
-function AreasCtrl($scope, $http, DTOptionsBuilder)
+function AreasCtrl($scope, $http)
 {
     $http.get('http://localhost:49915/api/vwAreas/SelectAll').success(function (data) {
         $scope.areas = data;
     }, function (error)  
     {  
         $log.error('Oops! Something went wrong while fetching the data.')  
+    });
+}
+
+function AreasinventarioCtrl($scope, $http)
+{
+    $http.get('http://localhost:49915/api/Equipo/SelectAll').success(function (data) {
+        $scope.equipos = data;
+    }, function (error) {
+        $log.error('Oops! Something went wrong while fetching the data.')
     });
 }
 
@@ -162,7 +167,8 @@ function MainCtrl($http) {
         { text: 'Washington' },
         { text: 'Sydney' },
         { text: 'Cairo' },
-        { text: 'Beijing' }
+        { text: 'Beijing' },
+        { text: 'Kalin' }
     ];
 
     /**
@@ -3771,10 +3777,9 @@ angular
     .controller('jstreeCtrl', jstreeCtrl)
     .controller('datamapsCtrl', datamapsCtrl)
     .controller('pdfCtrl', pdfCtrl)
-
-    .controller('addArea', addArea);
-
+    .controller('addArea', addArea)
     .controller('InventarioCtrl', InventarioCtrl)
-    .controller('EquiposCtrl', EquiposCtrl);
->>>>>>> 5569c53d6a9e14aa2f33a81e9636074e1b8ab840
+    .controller('EquiposCtrl', EquiposCtrl)
+    .controller('AreasinventarioCtrl', AreasinventarioCtrl);
+
 
